35abfdea2af473e9b92962044ab4714cc2d8f1de
yuvalrotenberg <yuval.rotenberg@e.braude.ac.il>
2026-01-17 11:15:19 +0200

    sms msg mock

---
+++ Assignment2Files/Bistro_Common/src/methods/CommonMethods.java
@@ -0,0 +1,14 @@
+package methods;
+
+public class CommonMethods {
+	
+	
+	/**
+	 * Mock send sms method to simulate the sending of 
+	 * some methods like register, reservadafsdas waiting lists
+	 **/
+	public static void sendSMSMock(StringBuilder sb) {
+		System.out.println("[SMS MOCK]"+sb.toString());
+	} 
+
+}
